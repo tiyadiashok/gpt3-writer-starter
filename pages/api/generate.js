e98c53532c54b558fc63d41571bfeacb0523562d
tiyadiashok <tiyadiashok1@gmail.com>
2023-03-08 15:14:38 +0530

    latest build

---
+++ pages/api/generate.js
@@ -0,0 +1,54 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+//const basePromptPrefix = `Guidelines to create a good email for `;
+const basePromptPrefix = `Generate a short and concise email for `
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+  res.status(200).json({ output: basePromptOutput });
+/*
+  const secondPrompt = 
+  `
+  Take the guidelines and topic of the email below and Generate a short and concise email
+
+  Topic: ${req.body.userInput}
+
+  Guidelines: ${basePromptOutput.text}
+
+  Email:
+  `
+  
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.7,
+		// I also increase max_tokens.
+    max_tokens: 250,
+  });
+  
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
+*/
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,39 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
+  const onUserChangedText = (event) => {
+    //console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
@@ -11,11 +42,41 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Email Message Generation Tool</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Enter the text of the email you're responding to or write a description of the message you'd like to create, and let our tool generate a polished email for you in seconds.</h2>
           </div>
+        </div>
+        <div className="prompt-container">
+          <textarea
+            className="prompt-box"
+            placeholder="Out of office sick"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a
+              className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+              {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className="badge-container grow">
